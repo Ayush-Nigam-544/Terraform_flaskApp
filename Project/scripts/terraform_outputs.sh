3392ff3750fdcc081982d97e844ac0fe1d63e1dc
Ayush-Nigam-544 <ayushnigam5444@gmail.com>
2025-03-02 17:26:48 +0530

    nothing

--- Project/scripts/terraform_outputs.sh
+++
@@ -1,3 +0,0 @@
-export ALB_DNS_NAME="my-alb-166970694.us-east-1.elb.amazonaws.com"
-export EC2_PUBLIC_IP="54.91.240.54"
-export RDS_ENDPOINT="terraform-20250301193516688500000001.cexgyec6uybj.us-east-1.rds.amazonaws.com:5432"
